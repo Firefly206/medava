d1dac181ff100f98f848d900c907b9cd682d70a3
Viet Bach Nguyen <174976740+Firefly206@users.noreply.github.com>
2025-11-23 10:07:09 -0500

    New changes

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -1,5 +1,30 @@
 package edu.uc.cs3003.medava;
 
 public class Medicine {
+    private String mMedicineName;
+    // Constructor
+    public Medicine(String medicineName) {
+        mMedicineName = medicineName;
+    }
+    // getter
+    public String getMedicineName() {
+       return mMedicineName;
+    }
+
+    public double minimumTemperature() { // virtual methods 
+        return 0.0;
+    }
+
+    public double maximumTemperature() { // virtual methods 
+        return 100.0;
+    }
+    // Check the medicine whether it remains safe if subjected to a particular temperature range.
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+            highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,11 @@
 package edu.uc.cs3003.medava;
+
+import java.util.List;
+
 public class Transporter {
+    private String mTransporterName;
+    private List<Medicine> goods;
+    public String getTransporterName() {
+        return mTransporterName;
+    }
 }
