01b8ed6d022716282df62bf58c52e2e44451a7c2
Viet Bach Nguyen <174976740+Firefly206@users.noreply.github.com>
2025-11-23 11:25:38 -0500

    New changes

--- src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
+++ src/main/java/edu/uc/cs3003/medava/Thrombolytic.java
@@ -1,1 +1,23 @@
 package edu.uc.cs3003.medava;
+
+public class Thrombolytic extends Medicine {
+    public Thrombolytic() {
+        super("Thrombolytic");
+    }
+
+    @Override // annotation. 
+    // provides an indication to the compiler about the programmer's intent
+    // and helps the compiler catch common errors. 
+    public double minimumTemperature() {
+        System.out.println("Getting the minimum safe temperature for a Thrombolytic drug.");
+        return 39.2;
+    }
+
+    @Override
+    public double maximumTemperature() {
+        System.out.println("Getting the maximum safe temperature for a Thrombolytic drug.");
+        return 41.0;
+    }
+
+
+}
--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -5,11 +5,18 @@ import java.util.List;
 
 public class Transporter {
     private String mTransporterName;
-    public List<Medicine> goods;
+    private List<Medicine> goods;
 
+    private double mLowTemperature, mHighTemperature;
     // instance initializers
     {
        goods = new ArrayList<Medicine>();
+    }
+    //constructor
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
     }
     public String getTransporterName() {
         return mTransporterName;
@@ -17,5 +24,19 @@ public class Transporter {
 
     public void ship() {
        // Do some shipping!
+    }
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
     }
 }
